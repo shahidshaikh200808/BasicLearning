df63645b5295dcfc2dc5e87a7f84aeb87b478b19
salimsheikh <salimsheikh4u2000@gmail.com>
2026-06-23 09:55:58 +0530

    adding javascript

--- CSS/Project/coffeescript.js
+++ CSS/Project/coffeescript.js
@@ -11,3 +11,31 @@ menuOpenButton.addEventListener("click", () => {
 // Close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //Responsive Breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});
