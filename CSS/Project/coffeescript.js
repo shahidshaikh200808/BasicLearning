020e0ed5931947c4fc5c70399fb000be4ec69dbd
salimsheikh <salimsheikh4u2000@gmail.com>
2026-07-08 09:24:22 +0530

    completed coffee website

--- CSS/Project/coffeescript.js
+++ CSS/Project/coffeescript.js
@@ -1,5 +1,6 @@
 "use strict"
 
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -7,9 +8,15 @@ const menuCloseButton = document.querySelector("#menu-close-button");
 menuOpenButton.addEventListener("click", () => {
   // Toggle mobile menu visibility 
   document.body.classList.toggle("show-mobile-menu")
-});
-// Close menu when the close button is clicked
-menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+}); 
+
+ // Close menu when the close button is clicked
+ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+ // Close menu when the nav link is clicked
+ navLinks.foreach(link => {
+  link.addEventListener("click", menuOpenButton.click());
+}),
 
 // Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
