ed5dafcd39d6308809c17295a87343a506f7f17c
salimsheikh <salimsheikh4u2000@gmail.com>
2026-07-09 09:47:18 +0530

    making barbershop website in steakhouse.html

---
+++ CSS/Project/steakhouse.js
@@ -0,0 +1,49 @@
+/*=============== SHOW & CLOSE MENU ===============*/
+
+
+/*=============== REMOVE MOBILE MENU ===============*/
+
+
+/*=============== CHANGE HEADER STYLES ===============*/
+
+
+/*=============== SWIPER WORKS ===============*/
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== GSAP ANIMATION ===============*/ 
+
+
+/* Home animation */
+
+
+/* Home background animation */
+
+
+/* The nav animation only works in the home section */
+
+
+/* About animation */
+
+
+/* Work animation */
+
+
+/* Service animation */
+
+
+/* Expert animation */
+
+
+/* Contact animation */
+
+
+/* Footer animation */
