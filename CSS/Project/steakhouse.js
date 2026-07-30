5506f918fc31b53451bc3d5e5a5a4037bdd4a91d
salimsheikh <salimsheikh4u2000@gmail.com>
2026-07-30 09:22:48 +0530

    designing  home section of barbershop website

--- CSS/Project/steakhouse.js
+++ CSS/Project/steakhouse.js
@@ -1,7 +1,7 @@
 /*=============== SHOW & CLOSE MENU ===============*/
-const navMenu = document.gatElementById('nav-menu'),
-      navToggle = document.gatElementById('nav-toggle'),
-      navClose = document.gatElementById('nav-close'),
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
 /* Show menu */
 if(navToggle){
@@ -18,7 +18,14 @@ if(navClose){
 }
 
 /*=============== REMOVE MOBILE MENU ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{  
+   const navMenu = document.getElementById('nav-menu')
+   //When we click on each nav__link,we renove the show menu class
+   navMenu.classList.remove('show-menu') 
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*=============== CHANGE HEADER STYLES ===============*/
 
